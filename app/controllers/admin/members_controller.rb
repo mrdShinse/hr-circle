d67e5d6fdad9ee1976d782dfb269e4c09a51e1f1
mrdShinse <shinse1128@hotmail.com>
2017-06-18 13:30:17 +0900

    refactor/rubocop like style

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,5 +1,6 @@
-class Admin::MembersController < ApplicationController
+# frozen_string_literal: true
 
+class AdminMembersController < ApplicationController # :nodoc:
   before_action :set_circle
 
   def index
@@ -7,13 +8,14 @@ class Admin::MembersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_circle
-      @circle = Circle.find_by!(id: params[:circle_id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def member_params
-      params.require(:person).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_circle
+    @circle = Circle.find_by!(id: params[:circle_id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def member_params
+    params.require(:person).permit(:name)
+  end
 end
